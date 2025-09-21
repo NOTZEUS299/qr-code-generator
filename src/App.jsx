4066250258de98c82e5efeaf0539179fec9b156f
NEXIAL <drshnptl137@gmail.com>
2025-09-21 14:56:14 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useContext, useState } from "react";
 import QrComponent from "./components/qrComponents/QrComponent";
-import QrContextProvider from "./store/qr-context";
 
 const App = () => {
+  const [rowCol, setRowCow] = useState({ row: 5, col: 5 });
+  const numberOfElem = rowCol.row * rowCol.col;
+
   return (
-    <>
-      <QrContextProvider>
-        <main className="w-full py-20 flex justify-center items-center relative">
-          <QrComponent />
-        </main>
-      </QrContextProvider>
-    </>
+    <div className={`p-5`}>
+      <main className="w-full py-20 flex justify-center items-center relative">
+        <QrComponent />
+      </main>
+      {/* <div className={`grid gap-2.5`} style={{ gridTemplateColumns: `repeat(${rowCol.col}, 1fr)` }}>
+        {Array.from({ length: numberOfElem }).map((x, i) => {
+          return (
+            <div className={`w-full bg-emerald-600 rounded-4xl flex justify-center items-center`} style={{ height: `${94 / rowCol.row}vh` }}>{i}</div>
+          )
+        })}
+      </div> */}
+    </div>
   );
 };
 

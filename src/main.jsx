b65bbc8c69e7ebf7522673d997787f449f2c43a1
NEXIAL <drshnptl137@gmail.com>
2025-09-21 14:56:20 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import QrContextProvider from "./store/qr-context";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QrContextProvider>
+      <App />
+    </QrContextProvider>
   </StrictMode>,
 )
